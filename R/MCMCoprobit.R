ab0149212045a2f05263c86eff27b888931f37f0
Andrew D. Martin <admartin@wustl.edu>
2010-05-11 18:43:21 +0000

    version 1.0-6

--- R/MCMCoprobit.R
+++ R/MCMCoprobit.R
@@ -32,9 +32,9 @@
     lecuyer.stream <- seeds[[3]]
     
     ## extract X, Y, and variable names from the model formula and frame 
-	call <- match.call() 	  
-	mt <- terms(formula, data=data)
-	if(missing(data)) data <- sys.frame(sys.parent())
+    call <- match.call() 	  
+    mt <- terms(formula, data=data)
+    if(missing(data)) data <- sys.frame(sys.parent())
     mf <- match.call(expand.dots = FALSE)
     mf$burnin <- mf$mcmc <- mf$b0 <- mf$B0 <- NULL
     mf$thin <- mf$... <- mf$tune <- mf$tdf <- mf$verbose <- mf$seed <- NULL
@@ -111,68 +111,68 @@
       stop("Please respecify and call MCMCoprobit() again.\n")
     }
 
-	## prior for alpha error checking
-	if(is.null(dim(a0))) {
-		a0 <- a0 * matrix(1, ncat-1, 1)  
-	}
-	if((dim(a0)[1] != ncat-1) || (dim(a0)[2] != 1)) {
-		cat("N(a0,A0) prior a0 not conformable.\n")
-		stop("Please respecify and call MCMCoprobit() again.\n") 
-	}   
-	if(is.null(dim(A0))) {
-		A0 <- A0 + diag(ncat - 1)    
-	}
-	if((dim(A0)[1] != ncat - 1) || (dim(A0)[2] != ncat - 1)) {
-		cat("N(a0, A0) prior A0 not conformable.\n")
-		stop("Please respecify and call MCMCoprobit() again.\n")
-	}
-		
+    ## prior for alpha error checking
+    if(is.null(dim(a0))) {
+      a0 <- a0 * matrix(1, ncat-1, 1)  
+    }
+    if((dim(a0)[1] != ncat-1) || (dim(a0)[2] != 1)) {
+      cat("N(a0,A0) prior a0 not conformable.\n")
+      stop("Please respecify and call MCMCoprobit() again.\n") 
+    }   
+    if(is.null(dim(A0))) {
+      A0 <- A0 + diag(ncat - 1)    
+    }
+    if((dim(A0)[1] != ncat - 1) || (dim(A0)[2] != ncat - 1)) {
+      cat("N(a0, A0) prior A0 not conformable.\n")
+      stop("Please respecify and call MCMCoprobit() again.\n")
+    }
+    
     ## form gamma starting values (note: not changeable)
     gamma <- matrix(NA,ncat+1,1)
     gamma[1] <- -300
     gamma[2] <- 0
     gamma[3:ncat] <- (polr.out$zeta[2:(ncat-1)] - polr.out$zeta[1])*.588
     gamma[ncat+1] <- 300
-		
+    
     ## posterior sample
     sample <- matrix(data=0, mcmc/thin, K + ncat + 1)
-
+    
     ## call C++ code to draw sample
-	nY <- as.matrix(as.numeric(Y))
-		
-	## mcmc.method
-	cowles <- as.integer(1)
-		if(mcmc.method[1]=="AC") {cowles <- as.integer(0)}
-	
-	## form the tuning parameter
-	tune <- vector.tune(tune, ncat-1)
+    nY <- as.matrix(as.numeric(Y))
+    
+    ## mcmc.method
+    cowles <- as.integer(1)
+    if(mcmc.method[1]=="AC") {cowles <- as.integer(0)}
+    
+    ## form the tuning parameter
+    tune <- vector.tune(tune, ncat-1)
     posterior <- NULL
-		
+    
     auto.Scythe.call(output.object="posterior", cc.fun.name="MCMCoprobit",
                      sample.nonconst=sample, Y=as.integer(Y), nY=nY, X=X, 
                      burnin=as.integer(burnin),
                      mcmc=as.integer(mcmc), thin=as.integer(thin),
                      tune=tune, tdf=as.double(tdf), 
-					 lecuyer=as.integer(lecuyer),
+                     lecuyer=as.integer(lecuyer),
                      seedarray=as.integer(seed.array),
                      lecuyerstream=as.integer(lecuyer.stream),
                      verbose=as.integer(verbose), beta=beta.start,
                      gamma=gamma, b0=b0, B0=B0, a0=a0, A0=A0, 
-					 cowles=as.integer(cowles)) 
+                     cowles=as.integer(cowles)) 
     
     ## put together matrix and build MCMC object to return
     sample <- matrix(posterior$sampledata, posterior$samplerow,
                      posterior$samplecol, byrow=FALSE)
-	if(mcmc.method[1]=="AC"){ 
-			sample[ , 1] <- sample[, 1] - sample[, K+2] ## post-MCMC normalization 
-			sample[ , (K+2):(K+ncat)] <- sample[ , (K+2):(K+ncat)] - sample[, K+2] ## post-MCMC normalization 
-		}
-	sample <- sample[, c(1:K, (K+3):(K+ncat))]
-
+    if(mcmc.method[1]=="AC"){ 
+      sample[ , 1] <- sample[, 1] - sample[, K+2] ## post-MCMC normalization 
+      sample[ , (K+2):(K+ncat)] <- sample[ , (K+2):(K+ncat)] - sample[, K+2] ## post-MCMC normalization 
+    }
+    sample <- sample[, c(1:K, (K+3):(K+ncat))]
+    
     output <- mcmc(data=sample, start=burnin+1, end=burnin+mcmc, thin=thin)
     xnames <- c(X.names, paste("gamma", 2:(ncat-1), sep=""))
     varnames(output) <- xnames
     attr(output, "title") <- "MCMCoprobit Posterior Sample"
-        
+    
     return(output)
   }
