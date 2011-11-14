3426e2c289e0642b0840a95c3d7f6f95ed29aafe
Jong Hee Park <jhp@uchicago.edu>
2011-11-14 19:00:58 +0000

    version 1.2-1

--- src/distributions.h
+++ src/distributions.h
@@ -5,7 +5,7 @@
  *
  * This program is free software; you can redistribute it and/or
  * modify under the terms of the GNU General Public License as
- * published by Free Software Foundation; either version 3 of the
+ * published by Free Software Foundation; either version 2 of the
  * License, or (at your option) any later version.  See the text files
  * COPYING and LICENSE, distributed with this source code, for further
  * information.
