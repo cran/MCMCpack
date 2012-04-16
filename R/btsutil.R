230dd4e37d59f5c17e20ddc1c721e00bd8d69328
Jong Hee Park <jhp@uchicago.edu>
2012-04-16 06:09:44 +0000

    version 1.2-3

--- R/btsutil.R
+++ R/btsutil.R
@@ -249,29 +249,3 @@
 
 
 
-## find the most reasonable model
-## by choosing a model with a larger break number
-## If the difference of natural log marginal likelihood is smaller than the threshold,
-## choose the model with a smaller break number
-## to avoid the over-identification problem
-"make.breaklist" <- function(BF, threshold=3){
-  eBF <- exp(BF)
-  N <- nrow(BF)
-  out <- rep(NA, N)
-  for (i in 1:N){
-    order.i <- order(eBF[i,], decreasing=TRUE)
-    if(eBF[i, order.i[1]] / eBF[i, order.i[2]] > threshold){
-      out[i] <- order.i[1]
-    }
-    else if (eBF[i, order.i[1]] / eBF[i, order.i[2]] < threshold & order.i[1]<=order.i[2]){
-      out[i] <- order.i[1]
-    }
-    else if (eBF[i, order.i[1]] / eBF[i, order.i[2]] < threshold & order.i[1]>order.i[2]){
-      out[i] <- order.i[2]
-    }
-    else{
-      cat("\n Error occurs at i = ", i)
-    }
-  }
-  return(out-1)
-}
